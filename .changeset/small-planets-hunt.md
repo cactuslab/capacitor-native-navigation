881a70e2f0c1dd3833573384cd3e570825a8acbd
Karl von Randow <karl@xk72.com>
2022-11-23 15:57:45 +1300

    iOS: move NativeNavigationViewController into its own file finally

---
+++ .changeset/small-planets-hunt.md
@@ -0,0 +1,5 @@
+---
+"@cactuslab/native-navigation": patch
+---
+
+iOS: move NativeNavigationViewController into its own file finally
---
+++ packages/plugin/ios/Plugin/NativeNavigationViewController.swift
@@ -0,0 +1,99 @@
+import Foundation
+import Capacitor
+
+class NativeNavigationViewController: UIViewController {
+
+    private weak var plugin: CAPPlugin!
+    var path: String {
+        didSet {
+            webViewNeedsUpdate = true
+        }
+    }
+    var state: JSObject? {
+        didSet {
+            webViewNeedsUpdate = true
+        }
+    }
+    var webView: WKWebView? {
+        willSet {
+            if let webView = webView {
+                webView.removeFromSuperview()
+            }
+        }
+        didSet {
+            if let webView = webView {
+                webView.translatesAutoresizingMaskIntoConstraints = false
+                self.view.addSubview(webView)
+                webView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+                webView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+                webView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+                webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+            }
+        }
+    }
+    var onDeinit: (() -> Void)?
+    private var viewReadyContinuations: [CheckedContinuation<Void, Never>] = []
+    private var webViewNeedsUpdate = false
+    private let stackId: ComponentId?
+
+    init(path: String, state: JSObject?, stackId: ComponentId?, plugin: CAPPlugin) {
+        self.path = path
+        self.state = state
+        self.stackId = stackId
+        self.plugin = plugin
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+        if let onDeinit = self.onDeinit {
+            onDeinit()
+        }
+    }
+
+    override var debugDescription: String {
+        return "\(super.debugDescription) componentId=\(componentId ?? "none") path=\(path)"
+    }
+
+    /**
+     Create the webview required for this view. Waits for the view to be ready before returning.
+     */
+    func createOpdateWebView() async {
+        guard webView == nil || webViewNeedsUpdate else {
+            return
+        }
+
+        await withCheckedContinuation { continuation in
+            self.viewReadyContinuations.append(continuation)
+
+            var notificationData: [String : Any] = ["path": self.path, "id": self.componentId!]
+            if let state = self.state {
+                notificationData["state"] = state
+            }
+            if let stackId = self.stackId {
+                notificationData["stack"] = stackId
+            }
+
+            if webView == nil {
+                /* Callback to JavaScript to trigger a call to window.open to create the WKWebView and then init it */
+                self.plugin.notifyListeners("createView", data: notificationData, retainUntilConsumed: true)
+            } else {
+                self.plugin.notifyListeners("updateView", data: notificationData, retainUntilConsumed: true)
+            }
+        }
+    }
+
+    func webViewReady() throws {
+        guard let continuation = viewReadyContinuations.first else {
+            throw NativeNavigatorError.illegalState(message: "View has already been reported as ready or has not been created")
+        }
+
+        viewReadyContinuations.removeFirst()
+
+        continuation.resume()
+    }
+
+}

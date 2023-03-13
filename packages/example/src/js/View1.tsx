b4d4ae02f08496a1412ea8eaf038a654fa6ea71d
Karl von Randow <karl@cactuslab.com>
2023-03-13 16:18:03 +1300

    example: add test for get()

--- packages/example/src/js/View1.tsx
+++ packages/example/src/js/View1.tsx
@@ -1,11 +1,12 @@
 import { NativeNavigation } from '@cactuslab/native-navigation'
 import { useNativeNavigationContext } from '@cactuslab/native-navigation-react'
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 export default function View1(): JSX.Element {
 	const { dismiss } = useNativeNavigationContext()
 	const location = useLocation()
+	const [got, setGot] = useState('')
 
 	const handleDismiss = useCallback(function(evt: React.MouseEvent) {
 		evt.preventDefault()
@@ -17,11 +18,17 @@ export default function View1(): JSX.Element {
 		NativeNavigation.reset()
 	}, [])
 
+	async function handleGet(evt: React.MouseEvent) {
+		evt.preventDefault()
+		const got = await NativeNavigation.get()
+		setGot(JSON.stringify(got, undefined, 2))
+	}
+
 	useEffect(function() {
 		return function() {
 			alert('View1 has been unmounted')
 		}
-	})
+	}, [])
 	
 	return (
 		<div>
@@ -39,7 +46,11 @@ export default function View1(): JSX.Element {
 			<h2>Unmounting</h2>
 			<p>Note that this view will show an alert when it is UNMOUNTED, so if it doesn't alert there is a fault!</p>
 			<p><button onClick={handleReset}>Reset</button></p>
+			<p><button onClick={handleGet}>Get state</button></p>
 			<p><Link to="/stack1">Go to Stack 1</Link></p>
+			{got && (
+				<pre>{got}</pre>
+			)}
 		</div>
 	)
 }

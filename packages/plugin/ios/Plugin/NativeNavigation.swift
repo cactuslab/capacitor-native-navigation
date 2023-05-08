cactuslab/capacitor-native-navigation

commit d0261dd4c6635ab398e99f139d4c8265742b9f71

iOS: Allow modals to be presented without a root view

packages/plugin/ios/Plugin/NativeNavigation.swift
@@ -156,16 +156,16 @@ class NativeNavigation: NSObject {
             container.view.addSubview(component.viewController.view)
             component.viewController.didMove(toParent: container)
         } else {
-            guard let top = try self.currentRoot() else {
-                throw NativeNavigatorError.illegalState(message: "Cannot find top")
-            }
-
             roots.append(component.componentId)
             
             component.viewController.modalPresentationStyle = options.style.toUIModalPresentationStyle()
             
             component.viewController.presentationController?.delegate = self
-            top.viewController.present(component.viewController, animated: options.animated)
+            if let top = try self.currentRoot() {
+                top.viewController.present(component.viewController, animated: options.animated)
+            } else {
+                self.bridge.viewController!.present(component.viewController, animated: options.animated)
+            }
         }
 
         return PresentResult(id: component.componentId)

commit 7fc322ce2c4e500df5e9797ae1cdccd49f0a10c6

iOS: fix crash regression from d0261dd4c6635ab398e99f139d4c8265742b9f71

packages/plugin/ios/Plugin/NativeNavigation.swift
@@ -156,12 +156,14 @@ class NativeNavigation: NSObject {
             container.view.addSubview(component.viewController.view)
             component.viewController.didMove(toParent: container)
         } else {
+            let top = try self.currentRoot()
+            
             roots.append(component.componentId)
             
             component.viewController.modalPresentationStyle = options.style.toUIModalPresentationStyle()
             
             component.viewController.presentationController?.delegate = self
-            if let top = try self.currentRoot() {
+            if let top = top {
                 top.viewController.present(component.viewController, animated: options.animated)
             } else {
                 self.bridge.viewController!.present(component.viewController, animated: options.animated)
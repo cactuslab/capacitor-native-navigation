157759331f5ab6a10025e6210a8c92edf1e848ad
Karl von Randow <karl@cactuslab.com>
2024-10-23 16:04:26 +1300

    chore: rollup: add missing external

--- packages/react-router/rollup.config.mjs
+++ packages/react-router/rollup.config.mjs
@@ -12,6 +12,7 @@ export default [
 		external: [
 			'capacitor-native-navigation',
 			'capacitor-native-navigation-react',
+			'capacitor-native-navigation-react/context',
 			'react',
 			'react-dom',
 			'react-router-dom',
